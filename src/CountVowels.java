950726260ecfdef3879639e08d55661ea6ff8872
gabin-AF <gabin@augmented-future.com>
2022-09-22 09:24:41 +0200

    countVowel proj incomplete

--- src/CountVowels.java
+++ src/CountVowels.java
@@ -3,9 +3,13 @@
 * For added complexity have it report a sum of each vowel found.
 * */
 
+import java.util.Scanner;
+
 public class CountVowels {
     public static void main(String[] args) {
         System.out.println("Counter Vowels App");
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
 
     }
 }
